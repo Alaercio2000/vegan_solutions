3e9866fce7772f8ef2d67fa4fa8b08a8e01db66c
Alaercio Vieira <alaercio.vieira@outlook.com>
2019-11-08 18:34:18 -0200

    Debugando o script home.js e arrumando as classes e id das tag na home.html

--- paginas/home/assets/js/home.js
+++ paginas/home/assets/js/home.js
@@ -1,81 +1,19 @@
-window.onscroll = function() {menuScroll() , iconeMenu() , textMenu1() , textMenu2(), textMenu3(), textMenu4(), textMenu5(), textMenu6(), textMenu7(), textMenu8() }
+window.onscroll = function () { menuScroll()}
+
+var elementosNav = document.querySelectorAll(".textMenu");
 
 function menuScroll() {
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("menuHeader").className = "bg-secondary";
-    } else {
-      document.getElementById("menuHeader").className = "";
-    }
+  if (document.documentElement.scrollTop > 410) {
+    document.getElementById("menuHeader").classList.add("bg-secondary");
+    document.getElementById("iconeMenu").classList.add("text-dark");
+    elementosNav.forEach(function(elem) {
+      elem.classList.add('text-dark');
+    })
+  } else {
+    document.getElementById("menuHeader").classList.remove("bg-secondary");
+    elementosNav.forEach(function(elem) {
+      elem.classList.remove('text-dark');
+    })
+    document.getElementById("iconeMenu").classList.remove("text-dark");
   }
-
-  function iconeMenu() {
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("iconeMenu").className = "material-icons text-dark";
-    } else {
-      document.getElementById("iconeMenu").className = "material-icons text-info";
-    }
-  }
-
-  function textMenu1(){
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("textMenu1").className = "nav-link text-dark font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    } else {
-      document.getElementById("textMenu1").className = "nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    }
-  }
-
-  function textMenu2(){
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("textMenu2").className = "nav-link text-dark font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    } else {
-      document.getElementById("textMenu2").className = "nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    }
-  }
-
-  function textMenu3(){
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("textMenu3").className = "nav-link text-dark font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    } else {
-      document.getElementById("textMenu3").className = "nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    }
-  }
-
-  function textMenu4(){
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("textMenu4").className = "nav-link text-dark font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    } else {
-      document.getElementById("textMenu4").className = "nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    }
-  }
-
-  function textMenu5(){
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("textMenu5").className = "nav-link text-dark font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    } else {
-      document.getElementById("textMenu5").className = "nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    }
-  }
-
-  function textMenu6(){
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("textMenu6").className = "nav-link text-dark font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    } else {
-      document.getElementById("textMenu6").className = "nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    }
-  }
-
-  function textMenu7(){
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("textMenu7").className = "nav-link text-dark font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    } else {
-      document.getElementById("textMenu7").className = "nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    }
-  }
-
-  function textMenu8(){
-    if (document.documentElement.scrollTop > 410) {
-      document.getElementById("textMenu8").className = "nav-link text-dark font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    } else {
-      document.getElementById("textMenu8").className = "nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4 linksMenu";
-    }
-  }
+}
