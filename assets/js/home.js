c65e8ac9647d3259c5ab0ecee8d8a09df575077c
megmiyata <megmiyata@hotmail.com>
2019-11-10 22:41:57 -0300

    cadastro e login profissional atualizado

---
+++ assets/js/home.js
@@ -0,0 +1,19 @@
+window.onscroll = function () { menuScroll()}
+
+var elementosNav = document.querySelectorAll(".textMenu");
+
+function menuScroll() {
+  if (document.documentElement.scrollTop > 410) {
+    document.getElementById("menuHeader").classList.add("bg-secondary");
+    document.getElementById("iconeMenu").classList.add("text-dark");
+    elementosNav.forEach(function(elem) {
+      elem.classList.add('text-dark');
+    })
+  } else {
+    document.getElementById("menuHeader").classList.remove("bg-secondary");
+    elementosNav.forEach(function(elem) {
+      elem.classList.remove('text-dark');
+    })
+    document.getElementById("iconeMenu").classList.remove("text-dark");
+  }
+}
