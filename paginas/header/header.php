79dee9b890a0c27bd5d148f10ebc62fb3f187e66
Alaercio2000 <alaercio.vieira@outlook.com>
2019-11-11 23:55:16 -0300

    Incluindo o css e javascript

--- paginas/header/header.php
+++ paginas/header/header.php
@@ -1,37 +1,43 @@
-
+<?php
+require("../login/modals.php");
+?>
+<script src="../login/assets/js/modal.js"></script>
+<link rel="stylesheet" href="../login/assets/css/modal.css">
+<link rel="stylesheet" href="../login/assets/css/forms.css">
 <header class="fixed-top">
-        <div id="menuHeader" class="container-fluid">
-            <div class="row flex-row-reverse flex-md-row">
-                <div class="col-6 col-pq-5 col-sm-8 col-md-2 pr-1">
-                    <a class="col" href="home.php">
-                        <img class="p-2 p-pq-1" height="90" src="../../assets/img/img-header-menu/logo.png" alt="Logo">
-                    </a>
-                </div>
-                <div class="col">
-                    <nav class="navbar navbar-expand-md">
-                        <div>
-                            <button id="botaoMenu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBar" aria-controls="navBar" aria-expanded="false" aria-label="Toggle navigation">
-                                <i id="iconeMenu" class="material-icons">
-                                    menu
-                                </i>
-                            </button>
-                        </div>
-                        <div id="navBar" class="nav collapse navbar-collapse">
-                            <ul class="navbar-nav mr-auto">
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Sobre</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Restaurantes</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Produtos</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Profissionais</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Blog</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Comunidade</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="javascript:void()" onClick="showModalLogin()">Acesse</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="javascript:void()" onClick="showModalCadastro()">Registre-se</a>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+    <div id="menuHeader" class="container-fluid">
+        <div class="row flex-row-reverse flex-md-row">
+            <div class="col-6 col-pq-5 col-sm-8 col-md-2 pr-1">
+                <a class="col" href="" onClick="window.location.href = window.location.href;">
+                    <img class="p-2 p-pq-1" height="90" src="../../assets/img/img-header-menu/logo.png" alt="Logo">
+                </a>
+            </div>
+            <div class="col">
+                <nav class="navbar navbar-expand-md">
+                    <div>
+                        <button id="botaoMenu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBar" aria-controls="navBar" aria-expanded="false" aria-label="Toggle navigation">
+                            <i id="iconeMenu" class="material-icons">
+                                menu
+                            </i>
+                        </button>
+                    </div>
+                    <div id="navBar" class="nav collapse navbar-collapse">
+                        <ul class="navbar-nav mr-auto">
+                            <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="../sobre/sobre.php">Sobre</a>
+                            <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="../search/search.php">Restaurantes</a>
+                            <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="../search/search.php/">Produtos</a>
+                            <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="../search/search.php">Profissionais</a>
+                            <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="../blog/blog.php">Blog</a>
+                            <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="../comunidade/comunidade.php">Comunidade</a>
+                            <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="javascript:void()" onClick="showModalLogin()">Acesse</a>
+                            <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="javascript:void()" onClick="showModalCadastro()">Registre-se</a>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         </div>
-    </header>
+    </div>
+</header>
 </body>
+
 </html>
