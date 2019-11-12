0444f4e1e86a09c672246b6d29ca979f3bf3702d
Alaercio2000 <alaercio.vieira@outlook.com>
2019-11-11 22:52:51 -0300

    Adicionando Header e footer externo

--- paginas/home/home.php
+++ paginas/home/home.php
@@ -18,41 +18,12 @@
 
 <body>
 
-    <header class="fixed-top">
-        <div id="menuHeader" class="container-fluid">
-            <div class="row flex-row-reverse flex-md-row">
-                <div class="col-6 col-pq-5 col-sm-8 col-md-2 pr-1">
-                    <a class="col" href="home.php">
-                        <img class="p-2 p-pq-1" height="90" src="assets/img/logo.png" alt="Logo">
-                    </a>
-                </div>
-                <div class="col">
-                    <nav class="navbar navbar-expand-md">
-                        <div>
-                            <button id="botaoMenu" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navBar" aria-controls="navBar" aria-expanded="false" aria-label="Toggle navigation">
-                                <i id="iconeMenu" class="material-icons">
-                                    menu
-                                </i>
-                            </button>
-                        </div>
-                        <div id="navBar" class="nav collapse navbar-collapse">
-                            <ul class="navbar-nav mr-auto">
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Sobre</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Restaurantes</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Produtos</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Profissionais</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Blog</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Comunidade</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="javascript:void()" onClick="showModalLogin()">Acesse</a>
-                                <a class="nav-link textMenu text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="javascript:void()" onClick="showModalCadastro()">Registre-se</a>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-            </div>
-        </div>
-    </header>
-
+    <?php
+    
+    require("../header/header.php");
+    
+    ?>
+    
     <div id="fundoBuscar" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active" data-interval="5000">
@@ -241,11 +212,11 @@
 
     </main>
 
-    <footer id="rodape" class="bg-dark text-light">
-        <div class="container">
-            <h5 class="p-3 text-center">@Copyright 2019 BeeVegan</h5>
-        </div>
-    </footer>
+    <?php
+    
+        require("../footer/footer.php");
+    
+    ?>
 
 </body>
 
