665c7079a27230fa5e63dfcb83e2eeea150b1c4f
Alaercio2000 <alaercio.vieira@outlook.com>
2019-11-11 22:55:10 -0300

    Adicionando footer externo para ser usados em todas as página

--- paginas/footer/footer.php
+++ paginas/footer/footer.php
@@ -1,11 +1,27 @@
-        <?php include 'modals.php'; ?>
+<footer id="rodape" class="text-light bg-dark">
 
-        <footer id="rodape" class="bg-dark text-light">
-            <div class="container">
-                <h5 class="p-3 text-center">@Copyright 2019 BeeVegan</h5>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6 col-md-2">
+                <a onClick="window.location.href = window.location.href;" href=""><img class="mt-2" src="../../assets/img/img-header-menu/logo.png" height="50" alt="Logo Veganices"></a>
             </div>
-        </footer>
+            <div class="col-6 d-none d-md-flex">
+                <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Sobre</a>
+                <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Produtos</a>
+                <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Profissionais</a>
+                <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Blog</a>
+                <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Comunidade</a>
+            </div>
+            <div class="col-6 col-md-4">
+                <h6 class="font-weight-bold pl-4 pt-2">Redes Sociais</h6>
+                <img height="40" class="mr-3 mt-3" src="../../assets/img/img-footer/facebook.png" alt="Logo Facebook">
+                <img height="40" class="mr-3 mt-3" src="../../assets/img/img-footer/instagram.png" alt="Logo Instagram">
+                <img height="40" class="mr-3 mt-3" src="../../assets/img/img-footer/twitter.png" alt="Logo Twitter">
+            </div>
+        </div>
+        <div class="col-12">
+            <h6 class="py-2 font-weight-bold">@Copyright 2019 BeeVegan</h6>
+        </div>
+    </div>
 
-    </body>
-
-</html>
+</footer>
