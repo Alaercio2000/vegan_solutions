288d59424ace5086443070da08d24d888e8239e0
Alaercio2000 <alaercio.vieira@outlook.com>
2019-11-14 18:06:26 -0300

    Colocando ação nos links

--- paginas/footer/footer.php
+++ paginas/footer/footer.php
@@ -3,20 +3,20 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-6 col-md-2">
-                        <a onClick="window.location.href = window.location.href;" href=""><img class="mt-2" src="../../assets/img/img-header-menu/logo.png" height="50" alt="Logo Veganices"></a>
+                        <a href="/veganices/paginas/home/home.php"><img class="mt-2" src="/veganices/assets/img/img-header-menu/logo.png" height="50" alt="Logo Veganices"></a>
                     </div>
                     <div class="col-6 d-none d-md-flex">
-                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Sobre</a>
-                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Produtos</a>
-                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Profissionais</a>
-                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Blog</a>
-                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="#">Comunidade</a>
+                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="/veganices/paginas/sobre/sobre.php">Sobre</a>
+                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="/veganices/paginas/search/search.php">Produtos</a>
+                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="/veganices/paginas/search/search.php">Profissionais</a>
+                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="/veganices/paginas/blog/blog.php">Blog</a>
+                        <a class="nav-link text-info font-weight-bold py-4 px-sm-1 px-md-2 px-lg-3 px-xl-4 pl-full-5 pr-full-4" href="/veganices/paginas/comunidade/comunidade.php">Comunidade</a>
                     </div>
                     <div class="col-6 col-md-4">
                         <h6 class="font-weight-bold pl-4 pt-2">Redes Sociais</h6>
-                        <img height="40" class="mr-3 mt-3" src="../../assets/img/img-footer/facebook.png" alt="Logo Facebook">
-                        <img height="40" class="mr-3 mt-3" src="../../assets/img/img-footer/instagram.png" alt="Logo Instagram">
-                        <img height="40" class="mr-3 mt-3" src="../../assets/img/img-footer/twitter.png" alt="Logo Twitter">
+                        <a target="_blank" href="https://facebook.com"><img height="40" class="mr-3 mt-3" src="/veganices/assets/img/img-footer/facebook.png" alt="Logo Facebook"></a>
+                        <a target="_blank" href="https://instagram.com"><img height="40" class="mr-3 mt-3" src="/veganices/assets/img/img-footer/instagram.png" alt="Logo Instagram"></a>
+                        <a target="_blank" href="https://twitter.com"><img height="40" class="mr-3 mt-3" src="/veganices/assets/img/img-footer/twitter.png" alt="Logo Twitter"></a>
                     </div>
                 </div>
                 <div class="col-12">
